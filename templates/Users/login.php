54cde1b3bf804a9a5b8f83c532f9e3fb0baf655b
Hanna <2024200804@student.uitm.edu.my>
2026-07-15 23:41:05 +0800

    Update login.php

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -58,10 +58,10 @@
         <p class="text-center text-xs text-slate-500 mt-6">
             Belum mempunyai akaun? 
             
-<a href="mailto:admin@talentroute.com?subject=Permohonan Akaun TalentRoute">
+        </p>
+        <a href="mailto:admin@talentroute.com?subject=Permohonan Akaun TalentRoute">
     Hubungi Admin
 </a>
-        </p>
     </div>
 
 </body>
