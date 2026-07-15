e1777e793084f5805e1a4d247df06bfdd34e3e7e
Hanna <2024200804@student.uitm.edu.my>
2026-07-15 23:03:44 +0800

    Update login.php

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -115,5 +115,19 @@
     </div>
 
 </div>
+
+    <script>
+
+function openModal() {
+    document.getElementById("adminModal").classList.remove("hidden");
+    document.getElementById("adminModal").classList.add("flex");
+}
+
+function closeModal() {
+    document.getElementById("adminModal").classList.remove("flex");
+    document.getElementById("adminModal").classList.add("hidden");
+}
+
+</script>
 </body>
 </html>
